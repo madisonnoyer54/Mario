b29dbd8c25bf6955229d6866f07657bedb872323
madisonnoyer <91191022+madisonnoyer@users.noreply.github.com>
2021-10-18 08:47:34 +0200

    ajout de main

---
+++ main.c
@@ -0,0 +1,11 @@
+/**
+ * \file main.c
+ * \author Juliette Schilling / Madison Noyer
+ * \brief Programme principale 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "constantes.h"
+#include "monde.h"
+#include "graphique.h"
