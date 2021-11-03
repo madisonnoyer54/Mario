ccf0c1838c23d12f167de9b55b24fc440ceecbee
noyer7u <madison.noyer2@etu.univ-lorraine.fr>
2021-11-03 22:21:14 +0100

    ajout des constante + des struct

--- constantes.h
+++ constantes.h
@@ -8,6 +8,54 @@
 #define CONSTANTES_H
 
 
+/**
+ * \brief Largeur de l'écran de jeu
+ */
+#define SCREEN_WIDTH 480
+
+
+/**
+ * \brief Hauteur de l'écran de jeu
+ */
+#define SCREEN_HEIGHT 300
+
+
+/**
+ * \brief Taille d'un vaisseau
+ */
+#define MARIO_SIZE 30
+
+
+/**
+ * \brief Taille d'un météorite
+ */
+#define WALL_SIZE 20
+
+
+/**
+ * \brief Largeur du paneau d'arrivée
+ */
+#define FINISH_PANEL_WIDTH 10
+
+
+/**
+ * \brief Pas de déplacement vertical de mario
+ */
+#define MOVING_STEP 10
+
+
+/**
+ * \brief Vitesse initiale de déplacement horizontale des éléments du jeu 
+ */
+#define INITIAL_SPEED 2
+
+
+/**
+ * \brief Nombre de murs 
+ */
+#define nb_walls 4
+
+
 
 
 
--- monde.h
+++ monde.h
@@ -8,4 +8,35 @@
 #ifndef MONDE_H
 #define MONDE_H
 
+/**
+ * \brief Représentation d'un sprite du jeu 
+ */
+struct sprite_s{
+    int x;/*!< Position (horizontale) de son centre */
+    int y;/*!< Position (verticale) de son centre */
+    int h;/*!< Hauteur */
+    int w;/*!< Largeur */
+};
+/**
+ * \brief Type qui correspont au sprite du jeu 
+ */
+typedef struct sprite_s sprite_t;
+
+
+/**
+ * \brief Représentation du monde du jeu
+ */
+struct world_s{
+    sprite_t mario; /*!< Champ qui correspond au sprite de mario */
+    sprite_t panel; /*!< Champ qui correspond au sprite du panneau d'arrivée */
+    sprite_t walls[nb_walls]; /*!< Champ qui correspond au tableau du sprites de murs */
+    sprite_t piege; /*!< Champ qui correspond au sprite du piège */
+};
+/**
+ * \brief Type qui correspond aux données du monde
+ */
+typedef struct world_s world_t;
+
+
+
 #endif
