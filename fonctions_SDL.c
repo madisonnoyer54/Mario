0e6ffbf7821b807835a9544c3e962aaf2a213bfd
noyer7u <madison.noyer2@etu.univ-lorraine.fr>
2021-11-07 11:36:47 +0100

    ajout des fonction charger image

--- fonctions_SDL.c
+++ fonctions_SDL.c
@@ -8,3 +8,26 @@
 #include "fonctions_SDL.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+
+SDL_Texture* charger_image (const char* nomfichier, SDL_Renderer*renderer){
+       
+        // Charger une image
+        SDL_Surface* surface = SDL_LoadBMP(nomfichier);
+        // Convertir la surface de l’image au format texture avant de l’appliquer
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface); 
+        //Libérer la surface
+        SDL_FreeSurface(surface);
+   
+        return texture; 
+}
+
+
+SDL_Texture* charger_image_transparente(const char* nomfichier, SDL_Renderer* renderer, Uint8 r, Uint8 g, Uint8 b){
+	SDL_Surface* surface = SDL_LoadBMP(nomfichier);
+	Uint32 key = SDL_MapRGB(surface->format, r,  g, b);
+	SDL_SetColorKey(surface, SDL_TRUE, key);
+	SDL_Texture* image = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+	return image;
+}
